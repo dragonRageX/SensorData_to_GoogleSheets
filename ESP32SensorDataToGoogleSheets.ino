49759300d9a6b0a9f9e22f37e1d75f5ea4509114
param <param772003@gmail.com>
2024-04-03 00:01:39 +0530

    first commit

---
+++ ESP32SensorDataToGoogleSheets.ino
@@ -0,0 +1,67 @@
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include "time.h"
+#include "WifiCredentialsAndGoogleAppScript.h"   //custom header file
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 19800;   //5hrs 30mins - IST
+const int   daylightOffset_sec = 0;
+// WiFi credentials
+// change SSID
+// change password
+// Google script ID and required credentials
+// change Gscript ID
+int count = 0;
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  delay(1000);
+  // connect to WiFi
+  Serial.println();
+  Serial.print("Connecting to wifi: ");
+  Serial.println(ssid);
+  Serial.flush();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+}
+void loop() {
+   if (WiFi.status() == WL_CONNECTED) {
+    static bool flag = false;
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+      Serial.println("Failed to obtain time");
+      return;
+    }
+    char timeStringBuff[50]; //50 chars should be enough
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+    String asString(timeStringBuff);
+    asString.replace(" ", "-");
+    Serial.print("Time:");
+    Serial.println(asString);
+    String urlFinal = "https://script.google.com/macros/s/" + GOOGLE_SCRIPT_ID + "/exec?" + "date=" + asString + "&sensor=" + String(count);
+    Serial.print("POST data to spreadsheet:");
+    Serial.println(urlFinal);
+    HTTPClient http;
+    http.begin(urlFinal.c_str());
+    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
+    int httpCode = http.GET(); 
+    Serial.print("HTTP Status Code: ");
+    Serial.println(httpCode);
+    //---------------------------------------------------------------------
+    //getting response from google sheet
+    String payload;
+    if (httpCode > 0) {
+        payload = http.getString();
+        Serial.println("Payload: "+payload);    
+    }
+    //---------------------------------------------------------------------
+    http.end();
+  }
+  count++;
+  delay(1000);
+}
